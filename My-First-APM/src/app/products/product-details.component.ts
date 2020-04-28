5b205c41299dbba72e34663afe171aed03d06d7b
bodhibrdr <julie.gumerman@gmail.com>
2020-04-28 17:12:54 -0600

    path to individual items created and working(ish)

--- My-First-APM/src/app/products/product-details.component.ts
+++ My-First-APM/src/app/products/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { IProduct } from './product';
 
 @Component({
@@ -9,9 +11,22 @@ export class ProductDetailsComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
   product: IProduct;
-  constructor() { }
+
+  constructor (private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-001',
+      'releaseDate': 'March 19, 2019',
+      'description': 'just a good old leaf rake',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png' 
+    }
   }
 
 }
